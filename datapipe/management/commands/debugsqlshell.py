150d5019ea0748f7c5c31f7de34135a4ede177e0
Jay Young <dev@yjmade.net>
2016-12-10 12:13:56 +0800

    [RM]unrelated command

--- datapipe/management/commands/debugsqlshell.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from .shell import Command as ShellCommand
-
-
-class Command(ShellCommand):
-    def handle(self,debug_sql=False,**options):
-        debug_sql=True
-        super(Command, self).handle(debug_sql,**options)
